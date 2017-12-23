1a6dd30901778d41999ef372e7e4d3193739a00e
Matti Aho <ookooko@gmail.com>
2017-12-23 08:45:46 +0200

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,12 +2,13 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'example@rc.com',
-    text: 'This is a uusi message'
-  });
 });
+
+//   socket.emit('createMessage', {
+//     from: 'example@rc.com',
+//     text: 'This is a uusi message'
+//   });
+// });
 
 socket.on('newMessage', function (message) {
   console.log('newMessage', message);
