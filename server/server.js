cfaaf06cf5c04e082564b466c532461125861d4e
Matti Aho <ookooko@gmail.com>
2017-12-23 10:12:30 +0200

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -21,6 +21,18 @@ io.on('connection', (socket) => {
   //   createdAt: 1231
   // });
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
     io.emit('newMessage', {
@@ -28,6 +40,11 @@ io.on('connection', (socket) => {
       text: message.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   // socket.on('disconnect', () => {
